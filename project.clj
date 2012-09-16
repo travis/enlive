75d008d32d08bc1db03a7c37aa1274b980e8284c
Travis Vachon <travis@copious.com>
2012-09-16 17:36:23 -0400

    Don't use version ranges in project.clj
    They're just not great:
    
    http://nelsonmorris.net/2012/07/31/do-not-use-version-ranges-in-project-clj.html

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject enlive "1.0.1"
   :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
   :url "http://github.com/cgrand/enlive/"
-  :dependencies [[org.clojure/clojure "[1.2.0,)"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.ccil.cowan.tagsoup/tagsoup "1.2"]])
