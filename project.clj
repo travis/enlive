ab8dee7a62bb10cb70e4ce935c04dd6c7963aedc
Travis Vachon <travis@copious.com>
2012-09-16 17:37:51 -0400

    Bump my version, change ns

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject enlive "1.0.1"
+(defproject tvachon/enlive "1.0.2"
   :description "a HTML selector-based (à la CSS) templating and transformation system for Clojure"
   :url "http://github.com/cgrand/enlive/"
   :dependencies [[org.clojure/clojure "1.2.0"]
